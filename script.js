7a347270b53ddfd4c403432fab579e95049789b9
Darby <dssumner@nic.edu>
2023-12-07 15:28:23 -0800

    Redoing nav bar

---
+++ script.js
@@ -0,0 +1,9 @@
+//responsive nav bar
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
