cfe8b94afe882033d6804d65fb18989cc23a608c
Darby <dssumner@nic.edu>
2023-12-08 13:56:09 -0800

    Had a problem with my CSS hamburger menu not working when I relinked files to their proper folders, trying to fix it.

--- script.js
+++
@@ -1,9 +0,0 @@
-//responsive nav bar
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
